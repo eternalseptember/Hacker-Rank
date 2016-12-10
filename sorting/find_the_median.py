53c7c78bf39ddd0fc9e803a48e0595f7877efe1b
KQ <eternal.september@gmail.com>
2016-12-09 19:20:09 -0500

    setting up test case

---
+++ sorting/find_the_median.py
@@ -0,0 +1,31 @@
+"""
+Given a list of numbers, can you find the median?
+
+Input Format:
+There will be two lines of input:
+n - the size of the array
+arr - n numbers that makes up the array
+
+Output Format:
+Output one integer, the median.
+"""
+
+
+def find_median(arr):
+	return arr[0]
+
+
+# n = int(input().strip())
+# arr = [int(temp) for temp in input().strip().split(' ')]
+
+# test case 1, expected answer: 3
+n = 7
+in_str1 = '0 1 2 4 6 5 3'
+
+arr = [int(temp) for temp in in_str1.strip().split(' ')]
+
+median = find_median(arr)
+print(median)
+
+
+
