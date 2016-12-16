679ef6fb4839090016c2372bbfbf5403440b63c5
KQ <eternal.september@gmail.com>
2016-12-16 12:40:07 -0500

    changed the way the sizes are calculated, but test case results remain the same

--- balanced_trees/median_updates_2.py
+++ balanced_trees/median_updates_2.py
@@ -10,59 +10,71 @@ from collections import Counter
 # min_heap on the right contains the larger half
 max_heap = Counter()
 min_heap = Counter()
+max_size = 0
+min_size = 0
 
 
 def median_update(op, x):
-	max_size = sum(max_heap.values())
-	min_size = sum(min_heap.values())
+	global max_size
+	global min_size
+	if max_size > 0:
+		max_heap_top = sorted(max_heap)[-1]
+	if min_size > 0:
+		min_heap_top = sorted(min_heap)[0]
 
 	if op == 'a':
 		# print('add: {0}'.format(x))
 		if (max_size == 0) and (min_size == 0):
 			# First item: put it in max_heap
 			max_heap[x] += 1
+			max_size += 1
 
-		elif (min_size == 0) and (x < sorted(max_heap)[-1]):
+		elif (min_size == 0) and (x < max_heap_top):
 			# Second item, scenario 1:
 			# transfer first item from max_heap to min_heap
-			transfer = sorted(max_heap)[-1]
-			min_heap[transfer] += 1
-			del max_heap[transfer]
+			del max_heap[max_heap_top]
+			min_heap[max_heap_top] += 1
+			min_size += 1
 
 			# then insert item in max_heap
 			max_heap[x] += 1
 
-		elif (min_size == 0) and (x >= sorted(max_heap)[-1]):
+		elif (min_size == 0) and (x >= max_heap_top):
 			# Second item, scenario 2: put it in min_heap
 			min_heap[x] += 1
+			min_size += 1
 
 		else:
 			# Third and after...
-			if x < sorted(max_heap)[-1]:
+			if x < max_heap_top:
 				max_heap[x] += 1
+				max_size += 1
 			else:
 				min_heap[x] += 1
+				min_size += 1
 
 		rebalance_heaps()
-		calc_median()
+		return calc_median()
 	else:
 		# print('remove: {0}'.format(x))
 		found = False
 
-		if (max_size > 0) and (x <= sorted(max_heap)[-1]):
+		if (max_size > 0) and (x <= max_heap_top):
 			# look in max_heap
 			if max_heap[x] > 0:
 				found = True
 				max_heap[x] -= 1
+				max_size -= 1
 
 				if max_heap[x] == 0:
 					del max_heap[x]
 
-		if (min_size > 0) and (x >= sorted(min_heap)[0]) and (found is False):
+		if (min_size > 0) and (x >= min_heap_top) and (found is False):
 			# look in min_heap
 			if min_heap[x] > 0:
 				found = True
 				min_heap[x] -= 1
+				min_size -= 1
 
 				if min_heap[x] == 0:
 					del min_heap[x]
@@ -70,20 +82,18 @@ def median_update(op, x):
 		# If the value is not in the list, print 'Wrong!'
 		# If the list is empty after removal, print 'Wrong!';
 		# otherwise, print the median.
-		max_size = sum(max_heap.values())
-		min_size = sum(min_heap.values())
 		if found is False:
-			print('Wrong!')
+			return None
 		elif (max_size == 0) and (min_size == 0):
-			print('Wrong!')
+			return None
 		else:
 			rebalance_heaps()
-			calc_median()
+			return calc_median()
 
 
 def rebalance_heaps():
-	max_size = sum(max_heap.values())
-	min_size = sum(min_heap.values())
+	global max_size
+	global min_size
 	diff = abs(max_size - min_size)
 
 	if diff > 1:
@@ -92,6 +102,9 @@ def rebalance_heaps():
 			transfer = sorted(min_heap)[0]
 			min_heap[transfer] -= 1
 			max_heap[transfer] += 1
+
+			min_size -= 1
+			max_size += 1
 
 			if min_heap[transfer] == 0:
 				del min_heap[transfer]
@@ -102,13 +115,16 @@ def rebalance_heaps():
 			max_heap[transfer] -= 1
 			min_heap[transfer] += 1
 
+			max_size -= 1
+			min_size += 1
+
 			if max_heap[transfer] == 0:
 				del max_heap[transfer]
 
 
 def calc_median():
-	max_size = sum(max_heap.values())
-	min_size = sum(min_heap.values())
+	global max_size
+	global min_size
 
 	if max_size == min_size:
 		median = (sorted(min_heap)[0] + sorted(max_heap)[-1]) / 2
@@ -118,11 +134,13 @@ def calc_median():
 		else:
 			median = sorted(max_heap)[-1]
 
-	print_median(median)
+	return median
 
 
 def print_median(i):
-	if (type(i) == int) or (int(i) == i):
+	if i is None:
+		print('Wrong!')
+	elif (type(i) == int) or (int(i) == i):
 		print(int(i))
 	else:
 		print('%.1f' % i)
@@ -137,6 +155,7 @@ in_str1 = ['r 1', 'a 1', 'a 2', 'a 1', 'r 1', 'r 2', 'r 1']
 for i in range(N):
 	# op, x = input().strip().split(' ')
 	op, x = in_str1[i].strip().split(' ')
-	median_update(op, int(x))
+	median = median_update(op, int(x))
+	print_median(median)
 
 # should be: Wrong!, 1, 1.5, 1, 1.5, 1, Wrong!
