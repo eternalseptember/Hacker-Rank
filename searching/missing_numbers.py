3e4a1a17b50022eb255fe97eec5e25a936f6148c
KQ <eternal.september@gmail.com>
2016-12-28 08:56:27 -0500

    missing numbers problem

---
+++ searching/missing_numbers.py
@@ -0,0 +1,46 @@
+"""
+Notes
+- If a number occurs multiple times in the lists, you must ensure that
+the frequency of that number in both lists is the same. If that is not
+the case, then it is also a missing number.
+- You have to print all the missing numbers in ascending order.
+- Print each missing number once, even if it is missing multiple times.
+- The difference between maximum and minimum number in B is less than
+or equal to 100.
+
+Input Format:
+There will be four lines of input:
+- n, the size of the first list
+- This is followed by n space-separated integers that make up the first list.
+- m, the size of the second list
+- This is followed by m space-separated integers that make up the second list.
+"""
+
+
+from collections import Counter
+
+# Setting up test case 1
+# Expected answer: 204 205 206
+
+n = 10
+in_str1 = '203 204 205 206 207 208 203 204 205 206'
+A = [int(temp) for temp in in_str1.strip().split(' ')]
+m = 13
+in_str2 = '203 204 204 205 206 207 205 208 203 206 205 206 204'
+B = [int(temp) for temp in in_str2.strip().split(' ')]
+
+
+"""
+n = int(input().strip())
+A = [int(temp) for temp in input().strip().split(' ')]
+m = int(input().strip())
+B = [int(temp) for temp in input().strip().split(' ')]
+"""
+
+listA = Counter(A)
+listB = Counter(B)
+listB.subtract(listA)
+
+for val in listB:
+	if listB[val] > 0:
+		print('{0}'.format(val), end=' ')
