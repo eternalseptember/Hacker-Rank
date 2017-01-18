20129c42f020bd3ca832bacedfbfb0610f3a9d8c
KQ <eternal.september@gmail.com>
2017-01-17 23:12:15 -0500

    more set up for merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,5 +1,6 @@
 """
-This problem doesn't actually exist in hackerrank.
+This problem doesn't actually exist in hackerrank, but there is a
+HR video on this topic from Gayle Laakmann McDowell.
 Using this as a basis for Merge Sort: Counting Inversions problem.
 """
 
@@ -11,5 +12,9 @@ def merge_sort(list_of_nums):
 
 
 
+in_str = '10 5 2 7 4 9 12 1 8 6 11 3'
 
+arr = [int(temp) for temp in in_str.strip().split(' ')]
+sorted = merge_sort(arr)
+print(sorted)
 
