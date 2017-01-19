14146db0ee4d849556e0594f457cde07a32161ef
KQ <eternal.september@gmail.com>
2017-01-18 22:25:40 -0500

    fleshing out merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -6,8 +6,43 @@ Using this as a basis for Merge Sort: Counting Inversions problem.
 
 
 def merge_sort(list_of_nums):
-	# stuff here
-	return list_of_nums
+	length = len(list_of_nums)
+	if length <= 1:
+		return list_of_nums
+
+	# setting up recursion
+	half = length // 2
+	half_1 = merge_sort(list_of_nums[:half])
+	half_2 = merge_sort(list_of_nums[half:])  # second half could be longer
+
+	sorted_list = []
+	half_1_item = None
+	half_2_item = None
+	done = False
+
+	while not done:
+		if (len(half_1) <= 0) or (len(half_2) <= 0):
+			done = True
+		else:
+			if half_1_item is None:
+				half_1_item = half_1.pop(0)
+			if half_2_item is None:
+				half_2_item = half_2.pop(0)
+
+			if half_1_item < half_2_item:
+				sorted_list.append(half_1_item)
+				half_1_item = None
+			else:
+				sorted_list.append(half_2_item)
+				half_2_item = None
+
+	# if one of the list still has items in it
+	if len(half_1) > 0:
+		sorted_list.extend(half_1)
+	elif len(half_2) > 0:
+		sorted_list.extend(half_2)
+
+	return sorted_list
 
 
 
