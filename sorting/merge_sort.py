c0f64e8863412bde7f7070a17b54796b02edb215
KQ <eternal.september@gmail.com>
2017-01-20 18:14:35 -0500

    moved merge function, trying to fix sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -12,45 +12,45 @@ def merge_sort(list_of_nums):
 
 	# setting up recursion
 	half = length // 2
-	half_1 = list_of_nums[:half]
-	half_2 = list_of_nums[half:]    # second half could be longer
+	left = list_of_nums[:half]
+	right = list_of_nums[half:]    # second half could be longer
 
 	if half > 1:
-		half_1 = merge_sort(half_1)
-		half_2 = merge_sort(half_2)
+		left = merge_sort(left)
+		right = merge_sort(right)
 
-	sorted_list = []
-	half_1_item = None
-	half_2_item = None
-	done = False
-
-	while not done:
-		if (len(half_1) <= 0) or (len(half_2) <= 0):
-			done = True
-		else:
-			if half_1_item is None:
-				half_1_item = half_1.pop(0)
-			if half_2_item is None:
-				half_2_item = half_2.pop(0)
-
-			if half_1_item < half_2_item:
-				sorted_list.append(half_1_item)
-				half_1_item = None
-			else:
-				sorted_list.append(half_2_item)
-				half_2_item = None
-
-	# if one of the list still has items in it
-	if len(half_1) > 0:
-		sorted_list.extend(half_1)
-	elif len(half_2) > 0:
-		sorted_list.extend(half_2)
-
-	return sorted_list
+	return merge(left, right)
 
 
 def merge(left, right):
 	merged = []
+
+	left_item = None
+	right_item = None
+	done = False
+
+	while not done:
+		if (len(left) <= 0) or (len(right) <= 0):
+			done = True
+		else:
+			if left_item is None:
+				left_item = left.pop(0)
+			if right_item is None:
+				right_item = right.pop(0)
+
+			if left_item < right_item:
+				merged.append(left_item)
+				left_item = None
+			else:
+				merged.append(right_item)
+				right_item = None
+
+	# if one of the list still has items in it
+	if len(left) > 0:
+		merged.extend(left)
+	elif len(right) > 0:
+		merged.extend(right)
+
 
 	return merged
 
