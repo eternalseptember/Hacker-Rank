02cb8f2ea5f5cb2d488354a282206a732d2c6517
KQ <eternal.september@gmail.com>
2017-02-01 19:06:50 -0500

    rename functions trying to fix merge and count

--- sorting/merge_sort_inversions.py
+++ sorting/merge_sort_inversions.py
@@ -11,11 +11,11 @@ respective values of arr_0, arr_1, ..., arr_n-1.
 
 
 def count_inversions(arr):
-	inv, sorted_arr = merge_sort(arr)
-	return inv
+	inv, sorted_arr = sort_and_count(arr)
+	return inv, sorted_arr
 
 
-def merge_sort(list_of_nums):
+def sort_and_count(list_of_nums):
 	length = len(list_of_nums)
 	if length <= 1:
 		return 0, list_of_nums
@@ -25,43 +25,47 @@ def merge_sort(list_of_nums):
 	left = list_of_nums[:half]
 	right = list_of_nums[half:]  # second half could be longer
 
-	left_inv, left = merge_sort(left)
-	right_inv, right = merge_sort(right)
-	merge_inv, merged = merge(left, right)
+	left_inv, left = sort_and_count(left)
+	right_inv, right = sort_and_count(right)
+	merge_inv, merged = merge_and_count(left, right)
 	inv_total = left_inv + right_inv + merge_inv
 
 	return inv_total, merged
 
 
-def merge(left, right):
+def merge_and_count(left, right):
 	merged = []
 
+	print('left: {0}  right: {1}'.format(left, right))
 	total_length = len(left) + len(right)
 	left_item = None
 	right_item = None
 	inv = 0
 
-	for i in range(total_length):
-		if (left_item is None) and (len(left) > 0):
+	while (len(left) > 0) and (len(right) > 0):
+		if left_item is None:
 			left_item = left.pop(0)
-		if (right_item is None) and (len(right) > 0):
+		if right_item is None:
 			right_item = right.pop(0)
 
-		if (left_item is None):
-			merged.append(right_item)
-			right_item = None
-		elif (right_item is None):
+		if left_item < right_item:
 			merged.append(left_item)
 			left_item = None
 		else:
-			if left_item < right_item:
-				merged.append(left_item)
-				left_item = None
-			else:
-				merged.append(right_item)
-				right_item = None
-				# inv += 1
-				inv += len(left)
+			merged.append(right_item)
+			right_item = None
+			inv += len(left)
+
+		print('len left: {0}  len right: {1}'.format(len(left), len(right)))
+
+	print('remaining lists. left: {0}  right: {1}'.format(left, right))
+
+	# now one list is empty
+	if len(left) == 0:
+		merged.extend(right)
+	elif len(right) == 0:
+		merged.extend(left)
+
 
 	return inv, merged
 
@@ -87,8 +91,9 @@ for i in range(d):
 	arr = [int(temp) for temp in inp.strip().split(' ')]
 	# arr = list(map(int, input().strip().split(' ')))
 
-	inversions = count_inversions(arr)
-	print(inversions)
+	#inversions = count_inversions(arr)
+	#print(inversions)
 
-
+	inversions, sorted_arr = count_inversions(arr)
+	print('{0}  {1}'.format(inversions, sorted_arr))
 
