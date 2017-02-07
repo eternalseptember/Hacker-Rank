be99fe5da5734c3416d2479404153e5d47ab85ea
KQ <eternal.september@gmail.com>
2017-02-06 19:16:14 -0500

    setting up first problem

---
+++ 10_days_of_statistics/mean_median_mode.py
@@ -0,0 +1,21 @@
+"""
+Given an array, X, of N integers, calculate and print the the respective mean,
+median, and mode on separate lines. If your array contains more than one modal
+value, choose the numerically smallest one.
+
+Your answers should be in decimal form, rounded to a scale of 1 decimal place.
+"""
+
+
+
+# N = int(input().strip())
+# arr = [int(temp) for temp in input().strip().split(' ')]
+
+
+N = 10
+in_str1 = '64630 11735 14216 99233 14470 4978 73429 38120 51135 67060'
+arr = [int(temp) for temp in in_str1.strip().split(' ')]
+
+print(arr)
+
+
